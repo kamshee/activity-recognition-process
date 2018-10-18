65c751a5a723dd3158a9f1d8b6ca8dae869766df
Joe Lee <kamshee@gmail.com>
2018-10-18 15:31:47 -0500

    added functions for data preprocessing

---
+++ PreprocessFunctions.py
@@ -0,0 +1,245 @@
+# These functions are based on the unstacked activity dictionary dataframe
+
+# import packages
+
+def unstack_ar_dictionary(act_dict):
+    """Takes a 4-level nested dictionary and unstacks into a activity recognition dataframe."""
+    df = pd.DataFrame([(k1, k2, k3, k4, v) for k1, k234v in act_dict.items()
+                           for k2, k34v in k234v.items()
+                           for k3, k4v in k34v.items()
+                           for k4, v in k4v.items()])
+    df.columns = ['task','trial','location','sensor','rawdata']
+    return df
+
+def power_spectra_welch_axis(rawdata,fm,fM):
+    """Compute PSD on each axis then combine into a dataframe"""
+    x = rawdata.iloc[:,0]
+    y = rawdata.iloc[:,1]
+    z = rawdata.iloc[:,2]
+    # number of samples in clip
+    n = len(x)
+    # sampling rate in clip
+    Fs = np.mean(1/(np.diff(x.index)/1000))
+    
+    # adjusted params to match frequency using scipy.welch with matlab.pwelch
+#     fx,Pxx_denX = welch(x,fs=30,nperseg=256,detrend=False)
+#     fy,Pxx_denY = welch(y,fs=30,nperseg=256,detrend=False)
+#     fz,Pxx_denZ = welch(z,fs=30,nperseg=256,detrend=False)
+    # added param: detrend=False
+    fx,Pxx_denX = welch(x,Fs,nperseg=min(256,n),detrend=False)
+    fy,Pxx_denY = welch(y,Fs,nperseg=min(256,n),detrend=False)
+    fz,Pxx_denZ = welch(z,Fs,nperseg=min(256,n),detrend=False)
+    
+    #return PSD in desired interval of freq
+    inds = (fx<=fM)&(fx>=fm)
+    f=fx[inds]
+    Pxx_denX=Pxx_denX[inds]
+    Pxx_denY=Pxx_denY[inds]
+    Pxx_denZ=Pxx_denZ[inds]
+    Pxx_den = {'PSD_X':Pxx_denX,'PSD_Y':Pxx_denY,'PSD_Z':Pxx_denY}
+    Pxxdf = pd.DataFrame(data=Pxx_den,index=f)
+
+    return Pxxdf
+
+def HPfilter_testclip(clip_data,cutoff=0.75,ftype='highpass'):
+    """
+    Highpass (or lowpass) filter data. HP to remove gravity (offset - limb orientation) from accelerometer 
+    data from each visit (trial)
+    
+    Input: Activity dictionary, cutoff freq [Hz], task, sensor location and type of filter 
+    (highpass or lowpass).
+    """
+    rawdata = clip_data
+#     if rawdata.empty is True: #skip if no data for current sensor
+#         continue
+    idx = rawdata.index
+    idx = idx-idx[0]
+    rawdata.index = idx
+    x = rawdata.values
+    Fs = np.mean(1/(np.diff(rawdata.index)/1000)) #sampling rate
+    #filter design
+    cutoff_norm = cutoff/(0.5*Fs)
+#     b,a = butter(4,cutoff_norm,btype=ftype,analog=False)
+# Matlab: change params to N=2, cutoff_norm= (prev 0.046875, cutoff=0.75)
+    b,a = butter(2,cutoff_norm,btype=ftype,analog=False)
+    #filter data
+    xfilt = filtfilt(b,a,x,axis=0)
+    rawdatafilt = pd.DataFrame(data=xfilt,index=rawdata.index,columns=rawdata.columns)
+    clip_data = rawdatafilt
+    return clip_data
+
+
+def featuretest(clip_data):
+    """
+    Extract features from a simple clip of a single trial and sensor
+    Input: simple clip without trial or sensor dict keys
+    Output: feature matrix from all clips from given subject and scores for each clip
+    Column names separate for acc and gyro data.
+    """
+    
+    features_list = ['meanX','meanY','meanZ','rangeX','rangeY','rangeZ','iqrX','iqrY','iqrZ',
+                     'stddev_X','stddev_Y','stddev_Z','skewX','skewY','skewZ','kurtX','kurtY','kurtZ',
+                     'hist1_X','hist2_X','hist3_X','hist4_X',
+                     'hist1_Y','hist2_Y','hist3_Y','hist4_Y',
+                     'hist1_Z','hist2_Z','hist3_Z','hist4_Z',
+                     #Moments of derivative: mean, SD, skew, kurtosis
+                     'mean_derivative_x','mean_derivative_y','mean_derivative_z',
+                     'std_derivative_x','std_derivative_y','std_derivative_z',
+                     'skew_derivative_x','skew_derivative_y','skew_derivative_z',
+                     'kurt_derivative_x','kurt_derivative_y','kurt_derivative_z',
+                     'mean_squared_norm','sum_stddev',
+                     'xcorr_XY','xcorr_XZ','xcorr_YZ',
+                     'crossprod_raw_xy','crossprod_raw_xz','crossprod_raw_yz',
+                     'crossprod_norm_xy','crossprod_norm_xz','crossprod_norm_yz',
+                     'abs_crossprod_raw_xy','abs_crossprod_raw_xz','abs_crossprod_raw_yz',
+                     'abs_crossprod_norm_xy','abs_crossprod_norm_xz','abs_crossprod_norm_yz',
+                     'PSD_mean_X','PSD_mean_Y','PSD_mean_Z',
+                     'PSD_std_X','PSD_std_Y','PSD_std_Z',
+                     'PSD_skew_X','PSD_skew_Y','PSD_skew_Z',
+                     'PSD_kur_X','PSD_kur_Y','PSD_kur_Z',
+                     # mean power 20 bins
+                     # x axis
+                     'meanpower_bin1_x','meanpower_bin2_x','meanpower_bin3_x','meanpower_bin4_x',
+                     'meanpower_bin5_x','meanpower_bin6_x','meanpower_bin7_x','meanpower_bin8_x',
+                     'meanpower_bin9_x','meanpower_bin10_x','meanpower_bin11_x','meanpower_bin12_x',
+                     'meanpower_bin13_x','meanpower_bin14_x','meanpower_bin15_x','meanpower_bin16_x',
+                     'meanpower_bin17_x','meanpower_bin18_x','meanpower_bin19_x','meanpower_bin20_x',
+                     # y axis
+                     'meanpower_bin1_y','meanpower_bin2_y','meanpower_bin3_y','meanpower_bin4_y',
+                     'meanpower_bin5_y','meanpower_bin6_y','meanpower_bin7_y','meanpower_bin8_y',
+                     'meanpower_bin9_y','meanpower_bin10_y','meanpower_bin11_y','meanpower_bin12_y',
+                     'meanpower_bin13_y','meanpower_bin14_y','meanpower_bin15_y','meanpower_bin16_y',
+                     'meanpower_bin17_y','meanpower_bin18_y','meanpower_bin19_y','meanpower_bin20_y',
+                     # z axis
+                     'meanpower_bin1_z','meanpower_bin2_z','meanpower_bin3_z','meanpower_bin4_z',
+                     'meanpower_bin5_z','meanpower_bin6_z','meanpower_bin7_z','meanpower_bin8_z',
+                     'meanpower_bin9_z','meanpower_bin10_z','meanpower_bin11_z','meanpower_bin12_z',
+                     'meanpower_bin13_z','meanpower_bin14_z','meanpower_bin15_z','meanpower_bin16_z',
+                     'meanpower_bin17_z','meanpower_bin18_z','meanpower_bin19_z','meanpower_bin20_z',]
+
+
+    #cycle through all clips for current trial and save dataframe of features for current trial and sensor
+    features = []
+
+    rawdata = clip_data
+
+    #range on each axis
+    min_xyz = np.min(rawdata,axis=0)
+    max_xyz = np.max(rawdata,axis=0)
+    r = np.asarray(max_xyz-min_xyz)
+
+    #Moments on each axis - mean, std dev, skew, kurtosis
+    mean = np.asarray(np.mean(rawdata,axis=0))
+    # np.std default ddof=0 as default so changed to 1 to match matlab
+    std = np.asarray(np.std(rawdata,axis=0, ddof=1))
+    sk = skew(rawdata)
+    # kurtosis has difference of +3 with matlab feature, thus the offset
+    kurt = kurtosis(rawdata)+3
+
+    #Cross-correlation between axes pairs
+    xcorr_xy = np.corrcoef(rawdata.iloc[:,0],rawdata.iloc[:,1])[0][1]
+    xcorr_xz = np.corrcoef(rawdata.iloc[:,0],rawdata.iloc[:,2])[0][1]
+    xcorr_yz = np.corrcoef(rawdata.iloc[:,1],rawdata.iloc[:,2])[0][1]
+    xcorr = np.array([xcorr_xy, xcorr_xz, xcorr_yz])
+
+    # interquartile range
+    iqrange = iqr(rawdata,axis=0)
+    
+    # histogram of z-score values
+    hist = rawdata-np.mean(rawdata)/np.std(rawdata,ddof=1)
+    hist_z_scores_x = np.histogram(hist.iloc[:,0],bins=4, range=(-2,2))
+    hist_z_scores_y = np.histogram(hist.iloc[:,1], bins=4, range=(-2,2))
+    hist_z_scores_z = np.histogram(hist.iloc[:,2], bins=4, range=(-2,2))
+    
+    hist_z_scores = np.concatenate((hist_z_scores_x[0], hist_z_scores_y[0], hist_z_scores_z[0]), axis=None)
+
+    # derivative - for 3 axis
+    derivative = np.diff(rawdata, axis=0)
+    # mean of derivative
+    mean_derivative = np.mean(derivative,axis=0)
+    # std dev of derivative
+    std_derivative = np.std(derivative,axis=0)
+    # skewness of derivative
+    skew_derivative = skew(derivative,axis=0)
+    # kurtosis of derivative
+        # added offset +3
+    kurt_derivative = kurtosis(derivative,axis=0)+3
+    moments_of_derivative = np.concatenate((mean_derivative,std_derivative,skew_derivative,kurt_derivative), axis=None)
+
+    # sum of xyz std dev
+    sum_stddev = np.array([np.sum(std)])
+
+    # mean of the squared norm
+    mean_squared_norm = np.array([np.mean(np.mean(np.square(rawdata)))])
+
+    # normalize values (divided by acc norm) to get cross products
+    # The norm should sum across xyz axis at each instance of time
+    normdenominator = np.ones((len(rawdata.columns),1))*np.array(np.sqrt(np.sum(np.square(rawdata),axis=1)))
+    norm = rawdata/normdenominator.T
+    
+    # cross products with raw and norm data
+    crossprod_norm_xy = np.nanmean(norm.iloc[:,0]*norm.iloc[:,1])
+    crossprod_norm_xz = np.nanmean(norm.iloc[:,0]*norm.iloc[:,2])
+    crossprod_norm_yz = np.nanmean(norm.iloc[:,1]*norm.iloc[:,2])
+    abs_crossprod_norm_xy = np.abs(crossprod_norm_xy)
+    abs_crossprod_norm_xz = np.abs(crossprod_norm_xz)
+    abs_crossprod_norm_yz = np.abs(crossprod_norm_yz)
+    crossprod_raw_xy = np.nanmean(rawdata.iloc[:,0]*rawdata.iloc[:,1])
+    crossprod_raw_xz = np.nanmean(rawdata.iloc[:,0]*rawdata.iloc[:,2])
+    crossprod_raw_yz = np.nanmean(rawdata.iloc[:,1]*rawdata.iloc[:,2])
+    abs_crossprod_raw_xy = np.abs(crossprod_raw_xy)
+    abs_crossprod_raw_xz = np.abs(crossprod_raw_xz)
+    abs_crossprod_raw_yz = np.abs(crossprod_raw_yz)
+    crossprod = np.array([crossprod_raw_xy, crossprod_raw_xz, crossprod_raw_yz,
+                          crossprod_norm_xy, crossprod_norm_xz, crossprod_norm_yz,
+                          abs_crossprod_raw_xy, abs_crossprod_raw_xz, abs_crossprod_raw_yz,
+                          abs_crossprod_norm_xy, abs_crossprod_norm_xz, abs_crossprod_norm_yz])
+    
+    # High pass filter before passing to PSD (to compare with Matlab process)
+    rawdata = HPfilter_testclip(rawdata)
+    
+    # power spectral density (PSD)
+# changed fm=0, to fm=1 like Andrew's code
+    Pxx = power_spectra_welch_axis(rawdata,fm=0,fM=10)
+    #moments of PSD
+    Pxx_moments = np.array([np.nanmean(Pxx.iloc[:,0].values),np.nanmean(Pxx.iloc[:,1].values),np.nanmean(Pxx.iloc[:,2].values),
+            np.nanstd(Pxx.iloc[:,0].values),np.nanstd(Pxx.iloc[:,1].values),np.nanstd(Pxx.iloc[:,2].values),
+            skew(Pxx.iloc[:,0].values),skew(Pxx.iloc[:,1].values),skew(Pxx.iloc[:,2].values),
+            kurtosis(Pxx.iloc[:,0].values),kurtosis(Pxx.iloc[:,1].values),kurtosis(Pxx.iloc[:,2].values)])
+########################
+    # Mean power in 0.5 Hz bins between 0 and 10 Hz (x, y, z)
+    binedges = np.arange(0,10.5,0.5)
+    powerbin_df = Pxx.groupby(pd.cut(Pxx.index, bins=binedges)).mean().fillna(0)
+    powerbinarray = np.concatenate((powerbin_df.iloc[:,0],powerbin_df.iloc[:,1],powerbin_df.iloc[:,2]), axis=None)
+
+#     # Andrew's mean PSD binning code
+#     #power spectra averaged within bins
+#     fm = 1; fM = 10; nbins = 10 #frequency bins
+# # figure x=Pxx ?
+#     Fs = np.mean(1/(np.diff(Pxx.index)/1000)) # sampling rate in clip
+#     n = Pxx.size
+#     timestep = 1/Fs
+#     bin1 = int(timestep*n*fm)
+#     bin2 = int(timestep*n*fM)
+#     bins = np.linspace(bin1,bin2,nbins,dtype=int) #sample indices
+#     deltab = int(0.5*np.diff(bins)[0]) #half the size of bin (in samples)
+#     Pxxm = []
+#     for i in bins:
+#         start = int(max(i-deltab,bins[0]))
+#         end = int(min(i+deltab,bins[-1]))
+#         Pxxm.append(np.mean(Pxx[start:end]))
+#     Pxxm = np.asarray(Pxxm)
+# #     plt.plot(bins/(timestep*n),Pxxm)
+#     powerbinarray = Pxxm
+########################################
+    #Assemble features in array
+    X = np.concatenate((mean,r,iqrange,std,sk,kurt,hist_z_scores,moments_of_derivative,
+                        mean_squared_norm,sum_stddev,xcorr,crossprod,Pxx_moments,powerbinarray))
+    features.append(X)
+    
+    F = np.asarray(features) #feature matrix for all clips from current trial
+
+    features = pd.DataFrame(data=F,columns=features_list,dtype='float32')
+
+    return features
+
